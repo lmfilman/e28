8b868ac12dc2e1c9407fcbd3a9221f820611a1bd
Laura Filmeyer <lmfilman@gmail.com>
2020-03-04 21:42:51 -0500

    meet requirements

--- scramble/index.js
+++ scramble/index.js
@@ -2,13 +2,14 @@ let app = new Vue({
   el: '#app',
   data: {
     answer: '',
-    answerIndex: 0,
+    answerIndex: -1,
     gameInitiated: false,
     guessIsSubmitted: false,
     hint: '',
-    mysteryWord: '',
+    outcomeText: '',
     playerGuess: '',
     playerName: '',
+    playerWon: false,
     words: [
       ['apple', 'Sometimes red, sometimes delicious'],
       ['washington', 'Rushmore’s left edge'],
@@ -27,16 +28,38 @@ let app = new Vue({
       return Math.floor(Math.random() * max);
     },
     resetGame: function() {
-      this.answerIndex = this.randomNumber(this.words.length);
+      var oldIndex = this.answerIndex;
+
+      // Ensure user does not see the same puzzle twice in a row
+      while(oldIndex == this.answerIndex) {
+        this.answerIndex = this.randomNumber(this.words.length);
+      }
+
       this.answer = this.words[this.answerIndex][0];
       this.hint = this.words[this.answerIndex][1];
-      this.mysteryWord = this.shuffleWord(this.answer);
       this.guessIsSubmitted = false;
+      this.outcomeText = '';
       this.playerGuess = '';
+      this.playerWon = false;
     },
-    shuffleWord: function(word) {
+    submitGuess: function() {
+      this.guessIsSubmitted = true;
+
+      if(this.playerGuess == this.answer) {
+        this.playerWon = true;
+      }
+
+      if(this.playerWon) {
+        this.outcomeText = 'You got it! Nice work.'
+      } else {
+        this.outcomeText = "Sorry, that's not correct. Please try again."
+      }
+    }
+  },
+  computed: {
+    mysteryWord: function() {
       // https://stackoverflow.com/questions/3943772/how-do-i-shuffle-the-characters-in-a-string-in-javascript
-      var a = word.split('');
+      var a = this.answer.split('');
       var n = a.length;
 
       for(var i = n - 1; i > 0; i--) {
@@ -46,15 +69,6 @@ let app = new Vue({
           a[j] = tmp;
       }
       return a.join('');
-    },
-
-    submitGuess: function() {
-      this.guessIsSubmitted = true;
-    }
-  },
-  computed: {
-    outcome: function() {
-      return this.playerGuess == this.answer;
     }
   },
   mounted: function () {
