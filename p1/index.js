b5d955f02556191a519f0e750c4c4457a7c7fe08
Laura Filmeyer <lmfilman@gmail.com>
2020-03-19 13:45:24 -0400

    P1 convert jquery to vue

--- p1/index.js
+++ p1/index.js
@@ -1,3 +1,35 @@
 let app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    gameStarted: false,
+    ids: ['rock', 'paper', 'scissors'],
+    outcomeDescription: '',
+    outcome: 0,
+    playerChoice: '',
+    randomChoice: ''
+  },
+  methods: {
+    selectChoice: function() {
+      this.gameStarted = true;
+
+      this.playerChoice = event.target.id;
+      this.randomChoice = this.ids[Math.floor(Math.random() * this.ids.length)];
+
+      let playerChoiceIndex = this.ids.indexOf(this.playerChoice);
+      let randomChoiceIndex = this.ids.indexOf(this.randomChoice);
+
+      let beatByIndex = (playerChoiceIndex + 1) % 3;
+
+      if (playerChoiceIndex == randomChoiceIndex) {
+        this.outcomeDescription = 'It was a tie, try again.';
+        this.outcome = 0;
+      } else if (randomChoiceIndex == beatByIndex) {
+        this.outcomeDescription = 'Sorry, you lost. :(';
+        this.outcome = -1;
+      } else {
+        this.outcomeDescription = 'You won! :)';
+        this.outcome = 1;
+      }
+    }
+  }
 })
