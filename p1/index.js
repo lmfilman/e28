9b9e7225987ab728e32030cf09020d4182f43c10
Laura Filmeyer <lmfilman@gmail.com>
2020-03-20 12:17:58 -0400

    show score, history, allow reset, use component

--- p1/index.js
+++ p1/index.js
@@ -1,22 +1,41 @@
+Vue.component('choice-selector', {
+  template: `
+    <button v-bind:id="choice" v-on:click="$emit('select-choice')">{{capitalizeStr(choice)}}</button>
+  `,
+  props: {
+    choice: String
+  },
+  methods: {
+    capitalizeStr: function(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+  }
+});
+
 let app = new Vue({
   el: '#app',
   data: {
     gameStarted: false,
-    ids: ['rock', 'paper', 'scissors'],
-    outcomeDescription: '',
+    history: [],
+    choices: ['rock', 'paper', 'scissors'],
     outcome: 0,
+    outcomeDescription: '',
     playerChoice: '',
     randomChoice: ''
   },
   methods: {
+    resetGame: function() {
+      this.gameStarted = false;
+      this.history = [];
+    },
     selectChoice: function() {
       this.gameStarted = true;
 
       this.playerChoice = event.target.id;
-      this.randomChoice = this.ids[Math.floor(Math.random() * this.ids.length)];
+      this.randomChoice = this.choices[Math.floor(Math.random() * this.choices.length)];
 
-      let playerChoiceIndex = this.ids.indexOf(this.playerChoice);
-      let randomChoiceIndex = this.ids.indexOf(this.randomChoice);
+      let playerChoiceIndex = this.choices.indexOf(this.playerChoice);
+      let randomChoiceIndex = this.choices.indexOf(this.randomChoice);
 
       let beatByIndex = (playerChoiceIndex + 1) % 3;
 
@@ -30,6 +49,30 @@ let app = new Vue({
         this.outcomeDescription = 'You won! :)';
         this.outcome = 1;
       }
+
+      this.history.push(this.outcome);
+    },
+    sumArray: function(arr) {
+      return arr.reduce(function(a, b) { return a + b; }, 0);
     }
+  },
+  computed: {
+    computerScore: function() {
+      function computerWon(score) {
+        return score < 0;
+      }
+
+      return -1 * this.sumArray(this.history.filter(computerWon));
+    },
+    playerScore: function() {
+      function playerWon(score) {
+        return score > 0;
+      };
+
+      return this.sumArray(this.history.filter(playerWon));
+    }
+  },
+  mounted: function () {
+    this.resetGame();
   }
 })
