5d4c9fe855b4a6a1ab9a24c15e2d107cb14e3873
Laura Filmeyer <lmfilman@gmail.com>
2020-04-20 22:08:40 -0400

    add new recipe page, doesn't seem to add

---
+++ p2/src/components/pages/RecipeCreatePage.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <h2>Add a Recipe</h2>
+
+    <label for='name'>Name</label>
+    <input type='text' v-model='recipe.name' id='name' />
+
+    <label for='id'>URL Identifier</label>
+    <input type='text' v-model='recipe.id' id='id' />
+
+    <label for='reference_type'>Reference Type (web, book, idea)</label>
+    <input type='text' v-model='recipe.reference_type' id='reference_type' />
+
+    <label for='source'>Source (website title, book title)</label>
+    <input type='text' v-model='recipe.source' id='source' />
+
+    <label for='reference'>Reference (url, page #)</label>
+    <input type='text' v-model='recipe.reference' id='reference' />
+
+    <label for='number_of_times_cooked'>Number of times cooked</label>
+    <input type='text' v-model='recipe.number_of_times_cooked' id='number_of_times_cooked' />
+
+    <label for='liked'>Liked?</label>
+    <input type='checkbox' v-model='recipe.liked' id='liked' />
+
+    <label for='ingredients'>Ingredients (comma separated)</label>
+    <input type='text' v-model='recipe.ingredients' id='ingredients' />
+
+    <label for='healthiness'>Healthiness (Low, Medium, High)</label>
+    <input type='text' v-model='recipe.healthiness' id='healthiness' />
+
+    <label for='categories'>Categories (comma separated)</label>
+    <input type='text' v-model='recipe.categories' id='categories' />
+
+    <input type='submit' value='Add' @click.prevent='addRecipe' />
+
+    <transition name='fade'>
+      <div class='alert' v-if='added'>Your product was added!</div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import * as app from '@/common/app.js';
+
+export default {
+  name: '',
+  data: function() {
+    return {
+      added: false,
+      recipe: {
+        name: '',
+        id: '',
+        reference_type: '',
+        source: '',
+        reference: '',
+        number_of_times_cooked: 0,
+        liked: false,
+        ingredients: '',
+        healthiness: '',
+        categories: ''
+      }
+    };
+  },
+  methods: {
+    addRecipe: function() {
+      app.api.add('recipes', this.product).then(id => {
+          console.log('Recipe was added with the id: ' + id);
+          this.added = true;
+          setTimeout(() => (this.added = false), 3000);
+          this.recipe = {
+            name: '',
+            id: '',
+            reference_type: '',
+            source: '',
+            reference: '',
+            number_of_times_cooked: 0,
+            liked: false,
+            ingredients: '',
+            healthiness: '',
+            categories: ''
+          }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+    font-size: 15pt;
+}
+textarea,
+input[type='text'] {
+    width: 50%;
+}
+textarea {
+    height: 75px;
+    display: block;
+    margin: auto;
+}
+label {
+    margin-top: 20px;
+    display: block;
+    font-weight: bold;
+}
+input[type='submit'] {
+    display: inline-block;
+    margin-top: 10px;
+}
+</style>
--- p2/src/main.js
+++ p2/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import App from '@/App.vue'
+import RecipeCreatePage from '@/components/pages/RecipeCreatePage.vue'
 import RecipesPage from '@/components/pages/RecipesPage.vue'
 import RecipePage from '@/components/pages/RecipePage.vue'
 import HomePage from '@/components/pages/HomePage.vue'
@@ -15,6 +16,7 @@ const routes = [
   { path: '/', component: HomePage, name: 'home' },
   { path: '/recipes', component: RecipesPage, name: 'recipes' },
   { path: '/recipes/:id', component: RecipePage, name: 'recipe', props: true },
+  { path: '/recipes/create', component: RecipeCreatePage, name: 'add a recipe' },
   { path: '/categories', component: CategoriesPage, name: 'categories' }
 ];
 
