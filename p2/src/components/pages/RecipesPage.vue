a3733d73db68f79c44c5ebfe926a4b691a29d394
Laura Filmeyer <lmfilman@gmail.com>
2020-04-23 18:20:04 -0400

    add category filter

--- p2/src/components/pages/RecipesPage.vue
+++ p2/src/components/pages/RecipesPage.vue
@@ -1,6 +1,14 @@
 <template>
   <div id='recipes'>
-    <div class='row recipe' v-for='recipe in recipes' :key='recipe.id'>
+    <div class="row">
+      <div class="column filter-category-label">
+        Category (any of):
+      </div>
+      <div class="column filter-category-select">
+        <multiselect v-model='selectedCategories' :options="categoryOptions" :multiple="true" :taggable="true" id='categories'></multiselect>
+      </div>
+    </div>
+    <div class='row recipe' v-for='recipe in recipesToDisplay' :key='recipe.id'>
       <div class='column recipe-name'>
         <div>{{ recipe.name }}</div>
       </div>
@@ -15,19 +23,43 @@
 
 <script>
 import * as app from '@/common/app.js';
+import Multiselect from 'vue-multiselect';
 
 export default {
   name: '',
+  components: { Multiselect },
   data: function() {
     return {
-      recipes: []
+      categoryOptions: [],
+      recipes: [],
+      selectedCategories: []
     };
+  },
+  methods: {
+    intersection: function(arr1, arr2) {
+      let set1 = new Set(arr1);
+      let set2 = new Set(arr2);
+      return new Set([...set1].filter(x => set2.has(x)));
+    }
   },
   mounted: function() {
     app.api.all('recipes').then(response => {
       let keys = Object.keys(response);
       this.recipes = keys.map(key => response[key]);
+
+      let categories = this.recipes.map(recipe => recipe.categories);
+      let mergedCategories = [].concat.apply([], categories);
+      this.categoryOptions = [...new Set(mergedCategories)].sort();
     });
+  },
+  computed: {
+    recipesToDisplay: function() {
+      if (this.selectedCategories.length == 0) {
+        return this.recipes;
+      } else {
+        return this.recipes.filter(recipe => this.intersection(recipe.categories, this.selectedCategories).size > 0);
+      }
+    }
   }
 };
 </script>
@@ -52,5 +84,16 @@ a {
 
 .recipe-link {
   width: 10%;
+}
+
+.filter-category-label {
+  margin-top: 10px;
+  padding-right: 0px;
+  width: 30%;
+}
+
+.filter-category-select {
+  width: 70%;
+  margin-bottom: 10px;
 }
 </style>
