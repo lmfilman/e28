e49b9a6aa48ef626d9cec6872129c9ae449fde2e
Laura Filmeyer <lmfilman@gmail.com>
2020-04-22 23:03:36 -0400

    find random recipe

--- p2/src/components/pages/HomePage.vue
+++ p2/src/components/pages/HomePage.vue
@@ -1,25 +1,50 @@
 <template>
   <div>
-    <p>This app is for determining what to make for dinner given some recipes we like and some recipes we're interested in trying out.</p>
-
-    <show-featured :category='"Soup"'></show-featured>
+    <div>
+      <p>This app is for determining what to make for dinner given some recipes we like and some recipes we're interested in trying out.</p>
+    </div>
+    <div>
+      <button @click='findRandom'>Find random recipe</button>
+      <div class='random' v-if='showRandom'>
+        <router-link :to='{name: "recipe", params: {id: randomRecipe.id}}'>
+          <div class='recipe'>
+            <div class='recipe-name'>{{ randomRecipe.name }}</div>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import ShowFeatured from '@/components/ShowFeatured.vue'
+import * as app from '@/common/app.js';
 
 export default {
   name: '',
-  components: {
-    'show-featured': ShowFeatured
-  },
   data: function() {
     return {
+      randomRecipe: null,
+      showRandom: false,
+      recipes: []
     };
+  },
+  methods: {
+    findRandom: function() {
+      this.showRandom = true;
+      this.randomRecipe = this.recipes[Math.floor(Math.random() * this.recipes.length)];
+    },
+  },
+  mounted: function() {
+    app.api.all('recipes').then(response => {
+      let keys = Object.keys(response);
+      this.recipes = keys.map(key => response[key]);
+    });
   }
 };
 </script>
 
 <style scoped>
+.random {
+  margin-top: 20px;
+}
 </style>
